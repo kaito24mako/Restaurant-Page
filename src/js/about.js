5641faff1dae351ac06033b6d073e08b89bfe269
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-06 16:38:19 +1100

    style about tab

--- src/js/about.js
+++ src/js/about.js
@@ -6,8 +6,16 @@ export function createAboutTab() {
     aboutTab.classList.add('tab');
     aboutTab.id = 'aboutTab';
 
+    const aboutWrapper = document.createElement('div');
+    aboutWrapper.id = 'aboutWrapper';
+
+    const heading = document.createElement('p');
+    heading.id = 'heading';
+    heading.textContent = 'About Kanazawa Delights';
+
     const description = document.createElement('p');
-    description.textContent = 'Welcome to Kanazawa Delights. At our restaurant, we strive to provide authentic, delicious Japanese foods inspired from the beautiful city of Kanazawa. There is nothing quite like this.'
+    description.id = 'description';
+    description.textContent = 'Welcome to Kanazawa Delights. At our restaurant, we strive to provide authentic, delicious Japanese foods inspired from the beautiful city of Kanazawa. Even though this is a fake restaurant, we do wish for a Kanazawa-inspired restaurant to be developed worldwide. There truly would be nothing quite like it.'
 
     const achievements = document.createElement('div');
     achievements.id = 'achievements';
@@ -15,11 +23,12 @@ export function createAboutTab() {
     const googleReview = document.createElement('p');
     const locations = document.createElement('p');
     const michelin = document.createElement('p');
-    googleReview.textContent = '4.8 stars out of 5 on Google Reviews';
-    locations.textContent = 'Stores in 50+ countries';
-    michelin.textContent = '5-star Michelin Restaurant';
+    googleReview.textContent = '⭐ 4.8 out of 5 on Google Reviews';
+    locations.textContent = '🌍 Stores in 50+ countries';
+    michelin.textContent = '🥘 5-star Michelin Restaurant';
 
     tabContainer.appendChild(aboutTab);
-    aboutTab.append(description, achievements);
+    aboutTab.appendChild(aboutWrapper);
+    aboutWrapper.append(heading, description, achievements);
     achievements.append(googleReview, locations, michelin);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
-import '../style.css';
+import '../css/style.css';
+import '../css/pageLoad.css';
+import '../css/about.css';
 
 import { createHeader } from './pageLoad.js';
-
 import { createAboutTab } from './about.js';
 
 createHeader();
